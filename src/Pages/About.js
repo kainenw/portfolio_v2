ea359b8f83b998d12309e889092931db924f8d73
kainenw <108428180+kainenw@users.noreply.github.com>
2024-03-29 18:24:19 -0500

    add and format project iframes, .png images

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,70 +3,132 @@ import "./_Pages.css";
 function About() {
   return (
     <div className="About Page">
-      <h1>
-      Kainen <span>|| Nashville-Based UX Designer with a development background
-      </span></h1>
+      <h1>Kainen White</h1>
+      <h2>Nashville-Based UX Designer & Front-End Developer</h2>
       <div className="row">
         <img src={require("../img/headshot.png")} alt="Kainen's headshot" />
         <div>
           <h2>About Me</h2>
           <p>
-          I'm a UX/UI designer who bridges the gap between design and development. With my understanding of coding principles, I create designs that are both beautiful and technically feasible. Let's collaborate to build user-focused experiences that seamlessly transition from concept to reality.
+            I'm a UX/UI designer who bridges the gap between design and
+            development. With my understanding of coding principles, I create
+            designs that are both beautiful and technically feasible. Let's
+            collaborate to build user-focused experiences that seamlessly
+            transition from concept to reality.
           </p>
         </div>
       </div>
-      <div>
-        <ul>
-          <h2>My Work</h2>
-          <li>
+      <div id="section2">
+        <h2>My Work</h2>
+        <div className="row">
+          <img
+            className="icon"
+            src={require("../img/projects/design.png")}
+            alt="project 2"
+          />
+          <div>
             <ul>
               <h3>Design</h3>
-              <li>Sushi App: User-friendly interface designed to simplify the food ordering process for a local restaurant.</li>
-              <li>Portfolio v2: Redesign of my personal portfolio, showcasing my best work and demonstrating my evolving design skills.</li>
-              <li>Voting App: Intuitive app to guide voters through the election process, promoting engagement.</li>
+              <li>
+                Sushi App: User-friendly interface designed to simplify the
+                food ordering process for a local restaurant.
+              </li>
+              <li>
+                Portfolio v2: Redesign of my personal portfolio, showcasing my
+                best work and demonstrating my evolving design skills.
+              </li>
+              <li>
+                Voting App: Intuitive app to guide voters through the election
+                process, promoting engagement.
+              </li>
             </ul>
-          </li>
-          <li>
+          </div>
+        </div>
+        <div className="row">
+          <img
+            className="icon"
+            src={require("../img/projects/dev.png")}
+            alt="project 2"
+          />
+          <div>
             <ul>
               <h3>Development</h3>
-              <li>Portfolio v2: Building the redesign for my portfolio page, including developing a react app and deploying it with a personal domain name kainen.dev.</li>
-              <li>Reddit App: Streamlined Reddit browsing experience for improved efficiency and enjoyment, built with React and Redux.</li>
-              <li>Tonnetz Visualizer: Interactive application that visualizes harmonic concepts for musicians, enhancing their understanding.</li>
-              <li>Savings Calculator: Financial planning tool empowering users to achieve their financial goals, built with javascript and jquery.</li>
+              <li>
+                Portfolio v2: Building the redesign for my portfolio page,
+                including several portfolio projects.
+              </li>
+              <li>
+                Reddit App: Streamlined Reddit browsing experience for
+                improved efficiency and enjoyment, built with React and Redux.
+              </li>
+              <li>
+                Tonnetz Visualizer: Interactive application that visualizes
+                harmonic concepts for musicians, enhancing their
+                understanding.
+              </li>
+              <li>
+                Savings Calculator: Financial planning tool empowering users
+                to achieve their financial goals, built with javascript and
+                jquery.
+              </li>
             </ul>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
       <div>
-        <div id="section2">
+        <div>
           <ul>
-          <h2>Skills:</h2>
-            <li>Empathizing with users by creating empathy maps, personas, user stories, and user journey maps</li>
+            <h2>Skills:</h2>
+            <li>
+              Empathizing with users by creating empathy maps, personas, user
+              stories, and user journey maps
+            </li>
             <li>Defining user pain points</li>
-            <li>Ideating design solutions using Crazy Eights, How Might We, and competitive audits</li>
+            <li>
+              Ideating design solutions using Crazy Eights, How Might We, and
+              competitive audits
+            </li>
             <li>Creating wireframes and prototypes on paper and digitally</li>
-            <li>Developing mockups using visual design elements and principles</li>
+            <li>
+              Developing mockups using visual design elements and principles
+            </li>
             <li>Designing in Figma and Adobe XD</li>
             <li>Conducting interviews and usability studies</li>
-            <li>Considering accessibility at every point in the design process</li>
-            <li>Front-End: HTML, CSS, JavaScript, Responsive Design, Accessibility Standards</li>
-            <li>Additional: Version Control, Web Hosting, Familiarity with broader web development concepts</li>
+            <li>
+              Considering accessibility at every point in the design process
+            </li>
+            <li>
+              Front-End: HTML, CSS, JavaScript, Responsive Design, Accessibility
+              Standards
+            </li>
+            <li>
+              Additional: Version Control, Web Hosting, Familiarity with broader
+              web development concepts
+            </li>
           </ul>
         </div>
         <div>
           <ul>
-          <h2>Experience:</h2>
-            <li>Freelance Work: Collaborated with local businesses to improve their websites through both design and development.</li>
-            <li>Self-Directed Learning: Completed online courses and actively pursuing a deeper understanding of UX design and web development. </li>
+            <h2>Experience:</h2>
+            <li>
+              Freelance Work: Collaborated with local businesses to improve
+              their websites through both design and development.
+            </li>
+            <li>
+              Self-Directed Learning: Completed online courses and actively
+              pursuing a deeper understanding of UX design and web development.{" "}
+            </li>
           </ul>
         </div>
       </div>
       <div>
-          <h2>Let's Build Something Amazing Together!</h2>
-          <p>
-          Interested in collaborating on design-focused projects? Let's connect! Reach me on LinkedIn, GitHub, and Twitter. You can also reach
-            out via email at <a href="mailto:kainen.white@gmail.com">kainen.white@gmail.com</a>
-          </p>
+        <h2>Let's Build Something Amazing Together!</h2>
+        <p>
+          Interested in collaborating on design-focused projects? Let's connect!
+          Reach me on LinkedIn, GitHub, and Twitter. You can also reach out via
+          email at{" "}
+          <a href="mailto:kainen.white@gmail.com">kainen.white@gmail.com</a>
+        </p>
       </div>
     </div>
   );
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -7,7 +7,11 @@ function Projects(props) {
       <h1>Projects</h1>
       <div className="Design">
         <h2>Design</h2>
-        <p>I led these UX design efforst, conducting user research, generating user personas, journeys etc., creating wireframes, mockups and prototypes in Figma.</p>
+        <p>
+          I led these UX design efforts, conducting user research, generating
+          user personas, journeys, etc., creating wireframes, mockups and
+          prototypes in Figma.
+        </p>
         <div className="row">
           <img
             className="icon"
@@ -17,26 +21,46 @@ function Projects(props) {
           <div>
             <h2>Voting App:</h2>
             <p>
-            Intuitive app to guide voters through the election process, promoting engagement.
+              Intuitive app to guide voters through the election process,
+              promoting engagement.
             </p>
           </div>
-          <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQuML0B_NIOFOHzWFC7PDCfCe-cVadFESsRBgGD1QGkSmDoSO_YdG6I40m__aZV42uH5bJPLwoymawz/embed?start=false&loop=false&delayms=3000" title="uxdproject3" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vQuML0B_NIOFOHzWFC7PDCfCe-cVadFESsRBgGD1QGkSmDoSO_YdG6I40m__aZV42uH5bJPLwoymawz/embed?start=false&loop=false&delayms=3000"
+            title="uxdproject3"
+            frameborder="0"
+            width="960"
+            height="569"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
         </div>
         <div className="row">
           <img
             className="icon"
-            src={require("../img/projects/portfolio.jpg")}
+            src={require("../img/projects/portfolio.png")}
             alt="project 2"
           />
           <div>
             <h2>Portfolio v2:</h2>
             <p>
-              Redesign of my personal portfolio, showcasing my best work and demonstrating my evolving design skills.
+              Redesign of my personal portfolio, showcasing my best work and
+              demonstrating my evolving design skills.
             </p>
           </div>
-          <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQgeh1fdcX2zunOIxK9VxjGgWX5kHayIw13Eo9_m0ptGNNgRgYTvJmsjl_ru0ZMavnQC7LzevOV29AZ/embed?start=false&loop=false&delayms=3000" title="uxdproject2" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vQgeh1fdcX2zunOIxK9VxjGgWX5kHayIw13Eo9_m0ptGNNgRgYTvJmsjl_ru0ZMavnQC7LzevOV29AZ/embed?start=false&loop=false&delayms=3000"
+            title="uxdproject2"
+            frameborder="0"
+            width="960"
+            height="569"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
         </div>
-        <div className="row">
+        <div className="presentation">
           <img
             className="icon"
             src={require("../img/projects/sushi.png")}
@@ -45,10 +69,20 @@ function Projects(props) {
           <div>
             <h2>Sushi App:</h2>
             <p>
-            User-friendly interface designed to simplify the food ordering process for a local restaurant.
+              User-friendly interface designed to simplify the food ordering
+              process for a local restaurant.
             </p>
           </div>
-          <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRcI5sv8eHU2oQbEUZTQFbUkB74zvDGRAK0zn850VpRaTmKhODP61ik8ePNw9PnqV1hRPQlLQpgHGyP/embed?start=false&loop=false&delayms=3000"title="uxdproject1" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vRcI5sv8eHU2oQbEUZTQFbUkB74zvDGRAK0zn850VpRaTmKhODP61ik8ePNw9PnqV1hRPQlLQpgHGyP/embed?start=false&loop=false&delayms=3000"
+            title="uxdproject1"
+            frameborder="0"
+            width="960"
+            height="569"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
         </div>
       </div>
       <div className="Development">
@@ -56,16 +90,17 @@ function Projects(props) {
         <div className="row">
           <img
             className="icon"
-            src={require("../img/projects/portfolio.jpg")}
+            src={require("../img/projects/portfolio.png")}
             alt="project 4"
           />
           <div>
             <h2>Portfolio v2:</h2>
             <p>
-            Building the redesign for my portfolio page, including developing a react app and deploying it with a personal domain name kainen.dev.
+              Building the redesign for my portfolio page, including several
+              portfolio projects.
             </p>
+            <a href="https://github.com/kainenw/portfolio_v2">Github Repo</a>
           </div>
-          <a href="https://github.com/kainenw/portfolio_v2">Github Repo</a>
         </div>
         <div className="row">
           <img
@@ -76,10 +111,11 @@ function Projects(props) {
           <div>
             <h2>Reddit App:</h2>
             <p>
-            Streamlined Reddit browsing experience for improved efficiency and enjoyment, built with React and Redux.
+              Streamlined Reddit browsing experience for improved efficiency and
+              enjoyment, built with React and Redux.
             </p>
+            <a href="https://github.com/kainenw/reddit">Github Repo</a>
           </div>
-          <a href="https://github.com/kainenw/reddit">Github Repo</a>
         </div>
         <div className="row">
           <img
@@ -90,10 +126,11 @@ function Projects(props) {
           <div>
             <h2>Tonnetz Visualizer:</h2>
             <p>
-            Interactive application that visualizes harmonic concepts for musicians, enhancing their understanding.
+              Interactive application that visualizes harmonic concepts for
+              musicians, enhancing their understanding.
             </p>
+            <a href="https://github.com/kainenw/tonnetz">Github Repo</a>
           </div>
-          <a href="https://github.com/kainenw/tonnetz">Github Repo</a>
         </div>
         <div className="row">
           <img
@@ -104,10 +141,11 @@ function Projects(props) {
           <div>
             <h2>Savings Calculator:</h2>
             <p>
-            Financial planning tool empowering users to achieve their financial goals, built with javascript and jquery.
+              Financial planning tool empowering users to achieve their
+              financial goals, built with javascript and jquery.
             </p>
+            <a href="https://github.com/kainenw/scalc">Github Repo</a>
           </div>
-          <a href="https://github.com/kainenw/scalc">Github Repo</a>
         </div>
         <div>
           <ul>
@@ -116,8 +154,8 @@ function Projects(props) {
               repositories if applicable.
             </li>
             <li>
-              Highlight projects that demonstrate your creativity, problem-solving
-              skills, and knowledge of front-end technologies.
+              Highlight projects that demonstrate your creativity,
+              problem-solving skills, and knowledge of front-end technologies.
             </li>
           </ul>
         </div>
