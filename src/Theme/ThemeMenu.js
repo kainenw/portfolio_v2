0d2608f57ae6b833bdfe1ac801287e742f674e30
kainenw <108428180+kainenw@users.noreply.github.com>
2025-05-25 23:38:31 -0500

    fix theme menu to the bottom right corner

--- src/Theme/ThemeMenu.js
+++ src/Theme/ThemeMenu.js
@@ -1,35 +1,40 @@
-/** @jsxRuntime classic */
 import React from 'react'
-import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+
+const StyledThemeMenu = styled.button`
+  position: fixed;
+  bottom: 10%;
+  right: 7%;
+  height: 4rem;
+  width: 4rem;
+  border: 1px solid #404040;
+  border-radius: 20px;
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+  outline: none;
+
+  img {
+    margin: 1rem;
+    height: 2rem;
+    width: 2rem;
+  }
+`
 
 function ThemeMenu(props) {
   const { theme, changeTheme } = props
 
   const handleClick = () => {
     const nextTheme = theme === 'dark' ? 'light' : 'dark'
+    document.documentElement.setAttribute('data-theme', nextTheme)
     changeTheme(nextTheme)
     localStorage.setItem('__theme', nextTheme)
   }
 
   return (
-    <div
-      onKeyDown={handleClick}
-      onClick={handleClick}
-      role="button"
-      tabIndex="0"
-      css={css`
-        position: fixed;
-        bottom: 10%;
-        right: 7%;
-        height: 4rem;
-        width: 4rem;
-        border: 1px solid #404040;
-        border-radius: 20px;
-        cursor: pointer;
-      `}
-    >
+    <StyledThemeMenu onClick={handleClick} aria-label="Change Theme">
       <img src={require(`../img/themed/${theme}/theme-icon.png`)} alt="toggle theme button" />
-    </div>
+    </StyledThemeMenu>
   )
 }
 
