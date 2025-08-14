f0c822c0113f3eca1ba54e34bd2e0aeed1abdc26
kainenw <108428180+kainenw@users.noreply.github.com>
2025-08-14 12:47:48 -0500

    refactor: remove react-router and use next navigation

--- migration/portfolio_v2.5/src/index.js
+++ migration/portfolio_v2.5/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App/App";
 import reportWebVitals from "./_tests_/reportWebVitals";
@@ -10,14 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter
-        future={{
-          v7_startTransition: true,
-          v7_relativeSplatPath: true,
-        }}
-      >
-        <App />
-      </BrowserRouter>
+      <App />
     </HelmetProvider>
   </React.StrictMode>
 );
